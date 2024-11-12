d260a9f0c56f39884e2a3a42b88def75726a1a0a
Khang Tran <khangthk@gmail.com>
2024-11-12 17:18:36 +0700

    Add BuildSolutionBase class

---
+++ Panther/Commands/BuildSolutionBase.cs
@@ -0,0 +1,25 @@
+﻿using Panther.Helpers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Panther.Commands
+{
+    internal abstract class BuildSolutionBase : BuildTargetBase
+    {
+        protected bool IsRebuild = false;
+
+        protected override void OnCommandDone()
+        {
+            _ = OnCopyWhenDoneAsync();
+            PrintCompleted();
+        }
+
+        protected async Task CopyFolderAsync(string solutionFilePath, string folder, string dstPath)
+        {
+            var srcPath = Path.Combine(Directory.GetParent(solutionFilePath).FullName, folder);
+            await FileFolderHelper.CopyFolderAsync(srcPath, dstPath);
+        }
+
+        protected abstract Task OnCopyWhenDoneAsync();
+    }
+}
